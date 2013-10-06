4da0185a85029b39ce4c05d4486e879eeb9f56ca
justinkhchan <justinkhchan@gmail.com>
2013-10-06 03:26:54 -0700

    code cleanup and commenting

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -5,17 +5,14 @@ public class Sudoku {
 	private static Integer[][] sudokuGrid = new Integer[GRID_LENGTH][GRID_LENGTH];
 	
 	public Sudoku(int[][] grid) {
-		copyContents(grid, sudokuGrid);
-	}
-	
-	private static void copyContents(int[][] oldGrid, Integer[][] newGrid) {
 		for (int copyCount = 0; copyCount < GRID_LENGTH; copyCount++) {
 			for (int colCount = 0; colCount < GRID_LENGTH; colCount++) {
-				sudokuGrid[copyCount][colCount] = oldGrid[copyCount][colCount];
+				sudokuGrid[copyCount][colCount] = grid[copyCount][colCount];
 			}
 			//System.arraycopy(oldGrid[copyCount], 0, newGrid[copyCount], 0, oldGrid[copyCount].length);
 		}
 	}
+
 	
 	public int[][] returnGrid() {
 		int[][] newGrid = new int[GRID_LENGTH][GRID_LENGTH];
